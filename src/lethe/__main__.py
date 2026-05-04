fbcc7240d3c18c8f1994e75199b3e4ee6cfdab48
rafael_moreno <rafael_moreno@iislafe.es>
2026-05-04 15:34:56 +0200

    added configuration file support that can be overwritten through CLI

--- src/lethe/__main__.py
+++ src/lethe/__main__.py
@@ -5,7 +5,7 @@ import textwrap
 from itertools import groupby
 from operator import attrgetter
 from pathlib import Path
-
+import json
 import rich
 import typer
 import uuid7
@@ -31,14 +31,11 @@ from .ocr_deidentify import perform_ocr
 from .output_dir import copy_and_organize_parallel
 from .pseudo import PseudonymGenerator
 from .version import __version__
+from pydantic import BaseModel, Field
 
 INPUT_DIR: Path = Path("/input")
 OUTPUT_DIR: Path = Path("/output")
-
-
-cli = typer.Typer(add_completion=False)
-utils_cli = typer.Typer()
-cli.add_typer(utils_cli, name="utils", help="Additional utilities")
+CONFIG_FILE: Path = Path("/config/config.json")
 
 # Remove default handler
 logger.remove()
@@ -51,18 +48,57 @@ logger.add(
     "{function}:{line} - <level>{message}</level>",
 )
 
+cli = typer.Typer(add_completion=False)
+utils_cli = typer.Typer()
+cli.add_typer(utils_cli, name="utils", help="Additional utilities")
 
+# Input settings configuration
+class Settings(BaseModel):
+    site_id: str | None = None
+    project_id: str | None = None
+
+    input_dir: Path = Path("/input")
+    output_dir: Path = Path("/output")
+    pepper: str | None = None
+    bscan_dcm_deidentify: bool = True
+    dcm_deidentify: bool = True
+    pseudonymize: bool = False
+    ocr: bool = False
+    paddle_ocr: bool = False
+
+    threads: int = DEFAULT_CPU_THREADS
+    hierarchical: bool = True
+    verbose: bool = False
+    version: bool | None = None
+    pseudonym_prefix: str = "{site_id}_"
+    state_dir: str = str(DEFAULT_STATE_DIR)
+
+    @classmethod
+    def from_config_file(cls, path: Path):
+        if path.exists():
+            with open(path) as f:
+                return cls(**json.load(f))
+        return cls()
+    
+def merge_settings(config: Settings, cli: dict) -> Settings:
+    data = config.model_dump()
+
+    for key, value in cli.items():
+        if value is not None:
+            data[key] = value
+
+    return Settings(**data)
+
+# Other utils
 def _create_secret_key() -> str:
     u = uuid7.create().hex
     d = luhn.calc_check_digit(u, alphabet="0123456789abcdef")
     return f"{u}{d}"
 
-
 def _valid_secret_key(secret_key: str) -> bool:
     if len(secret_key) != 33:
         return False
     return luhn.is_valid(secret_key, alphabet="0123456789abcdef")
-
 
 def _make_pseudonym_generator(
     state_dir: str,
@@ -73,7 +109,6 @@ def _make_pseudonym_generator(
         f"{state_dir}/{site_id}",
         pseudonym_prefix.format(site_id=site_id),
     )
-
 
 def _header_info() -> str:
     return textwrap.dedent(
@@ -92,7 +127,6 @@ def _header_info() -> str:
     """,
     )
 
-
 def version_callback(value: bool):
     console = Console()
     if value:
@@ -106,13 +140,11 @@ def version_callback(value: bool):
         console.print(f"CPU threads: {DEFAULT_CPU_THREADS}")
         raise typer.Exit()
 
-
 @utils_cli.command(help="Create a new 'secret' key to use for anonymization")
 def secret():
     secret = _create_secret_key()
     console = Console()
     console.print(f"[bold][magenta]{secret}[/]")
-
 
 @utils_cli.command(
     help="Extract and print the unique Series descriptions from input DICOM files"
@@ -251,7 +283,6 @@ def series_info(
     console.print(f"Total count of unique Series: {total_count}", style="bold")
     console.print(f"Total count of DICOM files: {total_img_count}", style="bold")
 
-
 @utils_cli.command(
     help="Export the mappings from source patient ids to the pseunymized ones"
 )
@@ -309,83 +340,22 @@ def export_lookup(
     console.print()
     console.print(table)
 
+# Main functions
 @cli.command(help="Run the DICOM anonymization pipeline")
 def run(
-    ctx: typer.Context,
+    # Related to encryption
     site_id: Annotated[
-        str,
+        str | None,
         typer.Argument(
-            help="The SITE-ID provided by the EUCAIM Technical team",
-        ),
-    ],
+            help="The SITE-ID provided by the EUCAIM Technical team"
+        )
+    ] = None,
     project_id: Annotated[
-        str,
+        str | None,
         typer.Argument(
-            help="The PROJECT-ID provided by the DATA HOLDER team",
-        ),
-    ],
-    input_dir: Annotated[
-        Path,
-        typer.Argument(
-            help="Input directory to read DICOM files from", show_default=True
-        ),
-    ] = INPUT_DIR,
-    output_dir: Annotated[
-        Path,
-        typer.Argument(
-            help="Output directory to write processed DICOM files to",
-            show_default=True,
-        ),
-    ] = OUTPUT_DIR,
-    bscan_dcm_deidentify: Annotated[
-        bool,
-        typer.Option(
-            "--bs_hash/--no-bs_hash",
-            help=(
-                "Perform encryption of the patientIDs based on the BreastSCan scheme."
-                "Uses the RSNA CTP anonymizer and the custom script."
-            ),
-        ),
-    ] = True,
-    dcm_deidentify: Annotated[
-        bool,
-        typer.Option(
-            "--ctp/--no-ctp",
-            help=(
-                "Perform deidentification in the DICOM metadata in image files. "
-                "Uses the RSNA CTP anonymizer and the custom script"
-            ),
-        ),
-    ] = True,
-    pseudonymize: Annotated[
-        bool,
-        typer.Option(
-            "--pseudonymize",
-            help=(
-                "Perform pseudonymization by keeping a lookup table for patient ids in the `state-dir` folder."
-                "The generated pseudonyms will be of the form `{pseudonym_prefix}{number}`, "
-                "where the number is generated sequentially starting from 1 but reusing existing mappings."
-            ),
-        ),
-    ] = False,
-    ocr: Annotated[
-        bool,
-        typer.Option("--ocr", help="Perform OCR (using Tesseract OCR)"),
-    ] = False,
-    paddle_ocr: Annotated[
-        bool,
-        typer.Option(
-            "--paddle-ocr",
-            help="Perform OCR using PaddleOCR",
-        ),
-    ] = False,
-    threads: Annotated[
-        int,
-        typer.Option(
-            help="Number of threads that RSNA CTP and PaddleOCR (if enabled) will use",
-            show_default=True,
-        ),
-    ] = DEFAULT_CPU_THREADS,
+            help="The PROJECT-ID provided by the DATA HOLDER team"
+        )
+    ] = None,
     pepper: Annotated[
         str | None,
         typer.Option(
@@ -395,50 +365,149 @@ def run(
                 " This also enables 'pseudonymization', but in a diferrent way than the --pseudonymize flag:"
                 " the secret key given here will be used for hashing patient ids, so the generated pseudonyms"
                 " will be different than the ones generated with `--pseudonymize`."
-            ),
-        ),
+            )
+        )
+    ] = None,
+
+    # Options
+    bscan_dcm_deidentify: Annotated[
+        bool | None, 
+        typer.Option(
+        "--bs_hash/--no-bs_hash",
+        help=(
+                "Perform encryption of the patientIDs based on the BreastSCan scheme."
+                "Uses the RSNA CTP anonymizer and the custom script."
+            )
+        )
+    ] = None,
+    dcm_deidentify: Annotated[
+        bool | None, 
+        typer.Option(
+            "--ctp/--no-ctp",
+            help=(
+                    "Perform deidentification in the DICOM metadata in image files. "
+                    "Uses the RSNA CTP anonymizer and the custom script"
+                )
+            )
+    ] = None,
+    pseudonymize: Annotated[
+        bool | None, 
+        typer.Option(
+            "--pseudonymize",
+            help=(
+                "Perform pseudonymization by keeping a lookup table for patient ids in the `state-dir` folder."
+                "The generated pseudonyms will be of the form `{pseudonym_prefix}{number}`, "
+                "where the number is generated sequentially starting from 1 but reusing existing mappings."
+            )
+        )
+    ] = None,
+    ocr: Annotated[
+        bool | None,
+        typer.Option(
+            "--ocr", 
+            is_flag=True,
+            help="Perform OCR (using Tesseract OCR)"
+            )
+    ] = None,
+    paddle_ocr: Annotated[
+        bool | None, 
+        typer.Option(
+            "--paddle-ocr", 
+            is_flag=True,
+            help="Perform OCR using PaddleOCR",
+        )
+    ] = None,
+    threads: Annotated[
+        int | None, 
+        typer.Option(
+            help="Number of threads that RSNA CTP and PaddleOCR (if enabled) will use"
+        )
     ] = None,
     hierarchical: Annotated[
-        bool,
+        bool | None, 
         typer.Option(
-            "--hierarchical/--no-hierarchical",
+        "--hierarchical/--no-hierarchical",
             help=(
                 "Output files will be organized into a hierarchical "
                 "Patient / Study / Series folder structure using the anonymized UIDs "
                 "as the folder names"
-            ),
-        ),
-    ] = True,
+            )
+        )
+    ] = None,
     verbose: Annotated[
-        bool,
-        typer.Option("--verbose", "-v", help="Enable verbose logging"),
-    ] = False,
+        bool | None, 
+        typer.Option(
+            "--verbose", 
+            "-v",
+            help="Enable verbose logging", 
+            is_flag=True
+        )
+    ] = None,
     version: Annotated[
-        bool | None,
+        bool | None, 
         typer.Option(
             "--version",
             "-V",
             callback=version_callback,
             is_eager=True,
             help="Print version information",
-        ),
-    ] = None,
+    )] = None,
     pseudonym_prefix: Annotated[
-        str,
+        str | None, 
         typer.Option(
-            help="The prefix to use for the patient's pseudonym id. You can use it as a template, passing '{site_id}' somewhere in it",
-            show_default=True,
-        ),
-    ] = "{site_id}_",
+            help="The prefix to use for the patient's pseudonym id. You can use it as a template, passing '{site_id}'"
+        )
+    ] = None,
     state_dir: Annotated[
-        str,
+        str | None, 
         typer.Option(
             help="The directory to use for storing state like lookup tables",
-            show_default=True,
-        ),
-    ] = str(DEFAULT_STATE_DIR),
+            show_default=True, 
+        )
+    ] = None,
 ):
+    config_settings = Settings.from_config_file(CONFIG_FILE)
 
+    cli_values = {
+        "site_id": site_id,
+        "project_id": project_id,
+        "input_dir": None,
+        "output_dir": None,
+        "pepper":pepper,
+        "bscan_dcm_deidentify":bscan_dcm_deidentify,
+        "dcm_deidentify":dcm_deidentify,
+        "pseudonymize": pseudonymize,
+        "ocr": ocr,
+        "paddle_ocr": paddle_ocr,
+        "threads":threads,
+        "hierarchical":hierarchical,
+        "verbose": verbose,
+        "version":version,
+        "pseudonym_prefix":pseudonym_prefix,
+        "state_dir":state_dir
+    }
+
+    settings = merge_settings(config_settings, cli_values)
+    site_id = settings.site_id
+    project_id = settings.project_id
+    input_dir = settings.input_dir
+    output_dir = settings.output_dir
+    pepper = settings.pepper
+    bscan_dcm_deidentify = settings.bscan_dcm_deidentify
+    dcm_deidentify = settings.dcm_deidentify
+    pseudonymize = settings.pseudonymize
+    ocr = settings.ocr
+    paddle_ocr = settings.paddle_ocr
+    threads = settings.threads
+    hierarchical = settings.hierarchical
+    verbose = settings.verbose
+    version = settings.version
+    pseudonym_prefix = settings.pseudonym_prefix
+    state_dir = settings.state_dir
+
+    if not site_id or not project_id:
+        raise typer.BadParameter("site_id and project_id must be provided via CLI or config")
+    
     if paddle_ocr and ocr:
         rich.print(
             "[red][bold]Error:[/bold] Cannot use both PaddleOCR and TesseractOCR: please choose one, use --help for usage information[/red]"
